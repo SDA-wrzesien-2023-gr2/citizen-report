933cdd6909b407a7af41b6fe169a7f566651fbcd
Paweł Przybyła <pawel.przybyla@gmail.com>
2023-09-08 18:56:22 +0200

    Model Report updated - choices

--- citizen_report/report/models.py
+++ citizen_report/report/models.py
@@ -4,13 +4,34 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Report(models.Model):
+    CATEGORY_CHOICES = [
+        ('RBR', 'Roads and bridges'),
+        ('SWS', 'Sewer and waterworks'),
+        ('POW', 'Power supply'),
+        ('GAS', 'Gasworks'),
+        ('TEL', 'Telecommunication'),
+        ('GAR', 'Garbage disposal'),
+        ('CTR', 'City transport'),
+        ('HTH', 'Healthcare'),
+        ('EDU', 'Education'),
+        ('SAF', 'Public safety')
+    ]
+    STATUS_CHOICES = [
+        ('FR', 'Fresh report'),
+        ('CH', 'Checking'),
+        ('VD', 'Veryfied'),
+        ('AP', 'Approved'),
+        ('WP', 'Work in progres'),
+        ('EW', 'Evaluation of works'),
+        ('CD', 'Closed - done'),
+    ]
     title = models.CharField(max_length=120)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='images/', blank=True)
-    category = models.CharField(max_length=120) # choice
-    status = models.CharField(max_length=120) # choice
+    category = models.CharField(max_length=3, choices=CATEGORY_CHOICES)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='FR')
     user = models.ForeignKey(User, related_name='reports', on_delete=models.CASCADE)
     clerk = models.ForeignKey(User, related_name='assigned_reports', on_delete=models.CASCADE)
 
