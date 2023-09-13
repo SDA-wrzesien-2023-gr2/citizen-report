9c2f79f9a4aa9b8d68b1efe113ada84da025135e
Spikey <rafal.paciorek19@gmail.com>
2023-09-14 01:23:40 +0200

    Add views and templates for reset password

--- citizen_report/authSystem/views.py
+++ citizen_report/authSystem/views.py
@@ -4,6 +4,8 @@ from django.views import generic
 from django.views.generic.detail import DetailView
 from django.contrib.auth import get_user_model, update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
 
 from .forms import CustomUserCreationForm
 
@@ -15,7 +17,7 @@ class SignUp(generic.CreateView):
 
 
 class UserView(DetailView):
-    template_name = 'profile.html'
+    template_name = 'user.html'
 
     def get_object(self, **kwargs):
         return self.request.user
@@ -25,7 +27,7 @@ class PasswordChange(generic.FormView):
     model = get_user_model()
     form_class = PasswordChangeForm
     success_url = reverse_lazy("home")
-    template_name = "change_password.html"
+    template_name = "password_change.html"
 
     def get_form_kwargs(self):
         kwargs = super(PasswordChange, self).get_form_kwargs()
@@ -37,3 +39,12 @@ class PasswordChange(generic.FormView):
         update_session_auth_hash(self.request, form.user)
         return super(PasswordChange, self).form_valid(form)
 
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'user/password_reset.html'
+    email_template_name = 'user/password_reset_email.html'
+    subject_template_name = 'user/password_reset_subject'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      "Please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy("home")
