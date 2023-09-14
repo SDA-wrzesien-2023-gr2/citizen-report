f4a7ed389efdde7aa91a89548a693711b9eb4d55
Spikey <rafal.paciorek19@gmail.com>
2023-09-14 20:23:09 +0200

    Correct views and templates for reset password

--- citizen_report/authSystem/views.py
+++ citizen_report/authSystem/views.py
@@ -4,7 +4,7 @@ from django.views import generic
 from django.views.generic.detail import DetailView
 from django.contrib.auth import get_user_model, update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib.auth.views import PasswordResetView
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView
 from django.contrib.messages.views import SuccessMessageMixin
 
 from .forms import CustomUserCreationForm
@@ -17,7 +17,7 @@ class SignUp(generic.CreateView):
 
 
 class UserView(DetailView):
-    template_name = 'user.html'
+    template_name = 'profile.html'
 
     def get_object(self, **kwargs):
         return self.request.user
@@ -27,7 +27,7 @@ class PasswordChange(generic.FormView):
     model = get_user_model()
     form_class = PasswordChangeForm
     success_url = reverse_lazy("home")
-    template_name = "password_change.html"
+    template_name = "user/change_password.html"
 
     def get_form_kwargs(self):
         kwargs = super(PasswordChange, self).get_form_kwargs()
@@ -41,10 +41,16 @@ class PasswordChange(generic.FormView):
 
 
 class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
-    template_name = 'user/password_reset.html'
-    email_template_name = 'user/password_reset_email.html'
-    subject_template_name = 'user/password_reset_subject'
+    template_name = 'user/reset_password.html'
+    email_template_name = 'user/reset_password_email.html'
+    subject_template_name = 'user/reset_password_subject.txt'
     success_message = "We've emailed you instructions for setting your password, " \
                       "if an account exists with the email you entered. You should receive them shortly." \
                       "Please make sure you've entered the address you registered with, and check your spam folder."
     success_url = reverse_lazy("home")
+
+
+class ResetConfirmPasswordView(SuccessMessageMixin, PasswordResetConfirmView):
+    template_name = 'user/reset_password_confirm.html'
+    success_url = reverse_lazy("reset_password_complete")
+
--- citizen_report/citizen_report/settings.py
+++ citizen_report/citizen_report/settings.py
@@ -139,3 +139,11 @@ AUTHENTICATION_BACKENDS = (
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
+
+# email configs
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
+EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
