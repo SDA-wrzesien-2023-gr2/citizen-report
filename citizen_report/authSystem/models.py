c85737e0a8ad0cae70fb19839d2950d7176ee7e4
Spikey <rafal.paciorek19@gmail.com>
2023-09-14 20:43:16 +0200

    Correct pep8 in models

--- citizen_report/authSystem/models.py
+++ citizen_report/authSystem/models.py
@@ -67,7 +67,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'username'
     EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = ['email', ]
+    REQUIRED_FIELDS = ['email',]
 
     objects = UserManager()
 
--- citizen_report/citizen_report/settings.py
+++ citizen_report/citizen_report/settings.py
@@ -145,5 +145,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
-EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
