00839447f4c3782db9ba53ee97d7b1d43b348201
Paweł Przybyła <pawel.przybyla@gmail.com>
2023-09-19 23:14:07 +0200

    add receiver

--- citizen_report/notifications/models.py
+++ citizen_report/notifications/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 
 Report = 'report.Report'
 User = 'authSystem.User'
@@ -13,9 +14,15 @@ class Notification(models.Model):
     message = models.CharField(max_length=200)
     is_read = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.message
+
 
 # UNDER CONSTRUCTION:
-# @receiver(post_save, sender=Report)
-# def create_notification
-
+@receiver(post_save, sender=Report)
+def create_notification(sender, instance, **kwargs):
+    notification = Notification(report=instance, user=instance.user, sent_at=timezone.now,
+                                message='zmiana lub dodanie', is_read=False)
+    print(notification)
+    print(instance)
 
