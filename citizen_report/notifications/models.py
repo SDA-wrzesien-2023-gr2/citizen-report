baa4f6362781d84f58a9f426e8a74bdb17a3ff11
Paweł Przybyła <pawel.przybyla@gmail.com>
2023-09-19 20:44:19 +0200

    create model notitications

--- citizen_report/notifications/models.py
+++ citizen_report/notifications/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
-# Create your models here.
+Report = 'report.Report'
+User = 'authSystem.User'
+
+
 class Notification(models.Model):
-    pass
+    report = models.ForeignKey(Report, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    message = models.CharField(max_length=200)
+    is_read = models.BooleanField(default=False)
