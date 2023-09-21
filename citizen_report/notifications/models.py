2f4670951ce2a37a36690c4c8fcad497de6e4631
Paweł Przybyła <pawel.przybyla@gmail.com>
2023-09-22 00:58:18 +0200

    receiver working, flexible, email sending added

--- citizen_report/notifications/models.py
+++ citizen_report/notifications/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
+from django.core.mail import send_mail
 
 from authSystem.models import  User
 
@@ -19,14 +20,24 @@ class Notification(models.Model):
     def __str__(self):
         return self.message
 
+    def __repr__(self):
+        return self.report, self.user, self.sent_at, self.message, self.is_read
+
 
 # UNDER CONSTRUCTION:
 @receiver(post_save, sender=Report)
 def create_notification(sender, created, instance, update_fields, **kwargs):
-    message = f'new report {instance.title} added!' if created else f'report status or firlds:{update_fields} changed'
+    message = f'new report {instance.title} added!' if created else f'report status or fields:{update_fields} changed'
     notification = Notification(report=instance, user=instance.user, sent_at=timezone.now,
                                 message=message, is_read=False)
     notification.save()
+    clerk = instance.clerk
+    send_mail(
+        f'subject: {message}',
+        f'{notification.__repr__()}',
+        f'{clerk.email}',
+        [f'{instance.user.email}'],
+    )
     print(notification)
     print(instance)
     print(created)
