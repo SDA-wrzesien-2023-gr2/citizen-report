4015402a831197d5b45ff4e705f0e0d800923353
Paweł Przybyła <pawel.przybyla@gmail.com>
2023-09-26 23:56:13 +0200

    begin views creation

--- citizen_report/notifications/views.py
+++ citizen_report/notifications/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, ListView
+
+from models import Notification
+
 
 # Create your views here.
+
+# UNDER CONSTRUCTION!!!
+class NotificationCreateView(CreateView):
+    model = 'Notification'
+    template_name = 'TEMPLATE_NAME'
+
+class NotificationDetailView(DetailView):
+    model = 'Notification'
+    template_name = 'TEMPLATE_NAME'
+
+class NotificationListView(ListView):
+    model = 'Notification'
+    template_name = 'TEMPLATE_NAME'
