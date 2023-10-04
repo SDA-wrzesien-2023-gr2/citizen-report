488e2f0cfba0f4579a51af93f0bafca6747db02a
Spikey <rafal.paciorek19@gmail.com>
2023-10-04 19:24:37 +0200

    add messagge from login, reset and change password

--- citizen_report/authSystem/urls.py
+++ citizen_report/authSystem/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', views.LogIn.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/authSystem/login'), name='logout'),
     path("signup/", views.SignUp.as_view(), name="signup"),
     path("user/change_password/", views.PasswordChange.as_view(), name="change_password"),
